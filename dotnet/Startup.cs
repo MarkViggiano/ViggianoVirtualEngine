61a298e7d5115b921a89f56d7233ced33b2d824c
Flaymed <mvigg8@gmail.com>
2021-02-20 21:24:54 -0500

    Clean Up Routing

--- dotnet/Startup.cs
+++ dotnet/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Session;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.CookiePolicy;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.AspNetCore.Mvc;
@@ -47,7 +46,6 @@ namespace dotnet {
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
             app.UseMvc();
 
             var trackPackageRouteHandler = new RouteHandler(context => {
@@ -56,20 +54,24 @@ namespace dotnet {
             });
 
             var routeBuilder = new RouteBuilder(app, trackPackageRouteHandler);
-
-            routeBuilder.MapRoute("Track Package Route","package/{operation:regex(^track|create$)}/{id:int}");
-
-            routeBuilder.MapGet("hello/{name}", context => {
-              var name = context.GetRouteValue("name");
-              // The route handler when HTTP GET "hello/<anything>" matches
-              // To match HTTP GET "hello/<anything>/<anything>,
-              // use routeBuilder.MapGet("hello/{*name}"
-              return context.Response.WriteAsync($"Hi, {name}!");
-            });
-
+            RegisterRoutes(routeBuilder);
+            
             var routes = routeBuilder.Build();
             app.UseRouter(routes);
 
         }
+
+        public void RegisterRoutes(RouteBuilder routeBuilder) {
+          routeBuilder.MapRoute("Track Package Route","package/{operation:regex(^track|create$)}/{id:int}");
+
+          routeBuilder.MapGet("hello/{name}", context => {
+            var name = context.GetRouteValue("name");
+            // The route handler when HTTP GET "hello/<anything>" matches
+            // To match HTTP GET "hello/<anything>/<anything>,
+            // use routeBuilder.MapGet("hello/{*name}"
+            return context.Response.WriteAsync($"Hi, {name}!");
+          });
+        }
+
     }
 }
